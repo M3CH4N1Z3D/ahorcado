1c668e7deeb8af22b95a5b33c278fab65d56a39a
unknown <fmrigueros91@gmail.com>
2025-05-13 13:41:09 -0500

    Arreglado reset de jugadores

--- ahorcado-front/src/app/players/page.tsx
+++ ahorcado-front/src/app/players/page.tsx
@@ -1,38 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function resetPlayers() {
-  const [loading, setLoading] = useState(false);
+export default function ResetPlayers() {
+  const router = useRouter();
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  const handleRestPlayers = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${apiUrl}/api/players`, {
-        method: "DELETE",
-      });
-      if (response.ok) {
-        alert("Jugadores eliminados exitosamente.");
-      } else {
-        alert("Hubo un error al eliminar los jugadores.");
-      }
-    } catch (error) {
-      console.error(error);
-      alert("Error al conectarse con el servidor.");
+  const handleResetPlayers = async () => {
+    const response = await fetch(`${apiUrl}/api/players`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      alert("Jugadores eliminados exitosamente.");
+      router.push("/");
+    } else {
+      alert("Hubo un error al eliminar los jugadores.");
     }
-    setLoading(false);
   };
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen bg-[url('/fondo.png')] bg-cover">
-      <div className="flex flex-row gap-2">
-        <button
-          className="mt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-green-700 transition"
-          onClick={handleRestPlayers}
-        >
-          {loading ? "Eliminando..." : "Eliminar Jugadores"}
-        </button>
-      </div>
+    <div className="flex flex-col items-center justify-center md:flex-col min-h-screen bg-[url('/fondo.png')] bg-cover">
+      <button
+        className="mt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-green-700 transition w-[50%]"
+        onClick={handleResetPlayers}
+      >
+        RESET JUGADORES
+      </button>
     </div>
   );
 }
